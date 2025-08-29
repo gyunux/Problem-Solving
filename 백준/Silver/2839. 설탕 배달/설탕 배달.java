71dc9705de6a032bdb70dfab9a40c9167ca271ca
Kim dong gyun <67477254+gyunux@users.noreply.github.com>
2025-08-29 19:32:21 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,27 @@
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        while (n > 0){
+            if(n % 5 == 0){
+               cnt += n / 5;
+               break;
+            }
+            else{
+                if(n < 3){
+                    System.out.println(-1);
+                    return;
+                }
+                else{
+                    cnt += 1;
+                    n -= 3;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
